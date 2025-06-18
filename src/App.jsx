c5a039c33074e991c9cb4de18f1d0a0fadbc8696
somemohit <98733767+somemohit@users.noreply.github.com>
2025-06-18 21:39:02 +0530

    navbar ffoter

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router';
 import Home from './pages/Home';
 import ErrorPage from './pages/ErrorPage';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="*" Component={ErrorPage} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
